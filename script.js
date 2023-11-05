24d698763c4088c867dbaaeebbf6c76bf605d418
stefannpv <you@example.com>
2023-11-05 20:06:39 +0200

    .

---
+++ script.js
@@ -0,0 +1,55 @@
+// function textAdd() {
+//   const heading = document.getElementsByClassName("heading");
+//   heading.classList.remove("display-none")
+// }
+
+// function textDelete() {
+//   const heading = document.getElementsByClassName("heading");
+//   heading.classList.add("display-none")
+// }
+
+
+const heading = document.querySelectorAll(".heading")
+
+heading.forEach( () => {
+  return heading.classList.remove("display-none");
+});
+
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
+
